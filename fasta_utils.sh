f593a14b5d2f17ffad6a48ae95fcc2242231f112
Evie Wan <ewan@broadinstitute.org>
2022-12-16 16:46:58 -0800

    delete

--- fasta_utils.sh
+++
@@ -1,9 +0,0 @@
-# count the number of sequences/headers
-grep -c "^>" file.fa
-
-# add something to end all headers
-sed 's/>.*/&WHATEVERYOUWANT/' file.fa > outfile.fa
-
-# subset mitochondrial reads from fasta file
-samtools faidx ref.fa chrM > subset.fa
-
